842e023ddc849c93fef9f01203f4cef3b841ec0d
Catalina Carreto <isccatalinacarreto27@gmail.com>
2020-03-05 14:31:50 -0600

    feat: Enviar peticion Ajax, leer e imprimir los resultados

--- js/app.js
+++ js/app.js
@@ -1,21 +1,15 @@
 document.querySelector('#generar-nombre').addEventListener('submit', cargarNombres);
-
 //Llamando a Ajax e imprimir resultado
 function cargarNombres(e){
     e.preventDefault();
     //Leer las variables
     const origen = document.getElementById('origen');
     const origenSeleccionado = origen.options[origen.selectedIndex].value;
-
     const genero = document.getElementById('genero');
     const generoSeleccionado = genero.options[genero.selectedIndex].value;
-
     const cantidad = document.getElementById('numero').value;
-
-
     let url ='';
     url += 'http://uinames.com/api/?';
-
     //Si hay origen agregarlo a la URL
     if(origenSeleccionado !== ''){
         url += `region=${origenSeleccionado}&`;
@@ -28,6 +22,28 @@ function cargarNombres(e){
     if (cantidad !== '') {
         url += `amount=${cantidad}&`;
     }
-    
-    console.log(url);
+    //Conectar con Ajax
+    //Iniciar XMLHttpResquest
+    const xhr = new XMLHttpRequest();
+    //Abrimos la conexion
+    xhr.open('GET', url, true);
+    //Datos e impresion del template
+    xhr.onload = function(){
+        if(this.status === 200){
+            const nombres = JSON.parse(this.responseText);
+            //Generar el HTML
+            let htmlNombres = '<h2>Nombres Generados</h2>';
+            htmlNombres += '<ul class="lista">';
+            //Imprimir cada nombre
+            nombres.forEach(function(nombre){
+                htmlNombres += `
+                    <li>${nombre.name}
+                `;
+            })
+            htmlNombres += '</ul>';
+            document.getElementById('resultado').innerHTML = htmlNombres;
+        }
+    }
+    //Enviar el Request
+    xhr.send();
 }
